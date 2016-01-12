1ba2a2bb8287cdedd3b18c152c49c7e3b453d037
Eric Auger <eric.auger@redhat.com>
2016-01-12 10:58:32 +0100

    iommu: Add iommu_domain_msi_geometry and DOMAIN_ATTR_MSI_GEOMETRY
    Introduce a new DOMAIN_ATTR_MSI_GEOMETRY domain attribute. It enables
    to query the aperture of the IOVA window dedicated to MSIs and
    test whether the MSIs must be mapped with the IOMMU-MSI API.
    
    x86 IOMMUs will typically expose an MSI aperture matching the 1MB
    region [FEE0_0000h - FEF0_000h] corresponding to the the APIC
    configuration space and no support of the IOMMU-MSI API.
    
    On ARM, the support of the IOMMU-MSI API will be advertised. Aperture
    will refect the setting performed with the IOMMU-MSI API.
    
    A helper function is added to allow testing if the aperture is valid.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    Suggested-by: Alex Williamson <alex.williamson@redhat.com>
    
    ---
    v8 -> v9:
    - rename programmable into iommu_msi_supported
    - add iommu_domain_msi_aperture_valid
    
    v8: creation
    - deprecates DOMAIN_ATTR_MSI_MAPPING flag

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -52,6 +52,12 @@ struct iommu_domain_geometry {
 	bool force_aperture;       /* DMA only allowed in mappable range? */
 };
 
+struct iommu_domain_msi_geometry {
+	dma_addr_t aperture_start; /* First address used for MSI IOVA    */
+	dma_addr_t aperture_end;   /* Last address used for MSI IOVA     */
+	bool iommu_msi_supported;  /* Is MSI mapping supported?		 */
+};
+
 /* Domain feature flags */
 #define __IOMMU_DOMAIN_PAGING	(1U << 0)  /* Support for iommu_map/unmap */
 #define __IOMMU_DOMAIN_DMA_API	(1U << 1)  /* Domain for use in DMA-API
@@ -83,6 +89,7 @@ struct iommu_domain {
 	iommu_fault_handler_t handler;
 	void *handler_token;
 	struct iommu_domain_geometry geometry;
+	struct iommu_domain_msi_geometry msi_geometry;
 	void *iova_cookie;
 };
 
@@ -108,6 +115,7 @@ enum iommu_cap {
 
 enum iommu_attr {
 	DOMAIN_ATTR_GEOMETRY,
+	DOMAIN_ATTR_MSI_GEOMETRY,
 	DOMAIN_ATTR_PAGING,
 	DOMAIN_ATTR_WINDOWS,
 	DOMAIN_ATTR_FSL_PAMU_STASH,
@@ -327,6 +335,12 @@ static inline size_t iommu_map_sg(struct iommu_domain *domain,
 extern struct iommu_group *pci_device_group(struct device *dev);
 /* Generic device grouping function */
 extern struct iommu_group *generic_device_group(struct device *dev);
+
+static inline bool iommu_domain_msi_aperture_valid(struct iommu_domain *domain)
+{
+	return (domain->msi_geometry.aperture_end >
+		domain->msi_geometry.aperture_start);
+}
 
 #else /* CONFIG_IOMMU_API */
 
