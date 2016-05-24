15eecdd1edd4d17857304be46c24b7ded670999d
Eric Auger <eric.auger@redhat.com>
2016-05-24 09:27:14 +0000

    genirq/msi: Allow MSI doorbell (un)registration
    This new API aims at allowing irqchips to allocate & register
    the MSI doorbells likely to be iommu mapped.
    
    Later on, other services will be added allowing the VFIO layer
    to query information based on all registered doorbells.
    
    We count the number of doorbells that do not implement IRQ
    remapping. They will be considered as unsafe with respect to MSI
    assignment.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    
    ---
    v11 -> v12:
    - rename irqchip_doorbell into msi_doorbell, irqchip_doorbell_list
      into msi_doorbell_list and irqchip_doorbell_mutex into
      msi_doorbell_mutex
    - fix style issues: align msi_doorbell struct members, kernel-doc comments
    - use kzalloc
    - use container_of in msi_doorbell_unregister_global
    - compute nb_unsafe_doorbells on registration/unregistration
    - registration simply returns NULL if allocation failed
    
    v10 -> v11:
    - remove void *chip_data argument from register/unregister function
    - remove lookup funtions since we restored the struct irq_chip
      msi_doorbell_info ops to realize this function
    - reword commit message and title

---
+++ include/linux/msi-doorbell.h
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 Eric Auger
+ *
+ * Eric Auger <eric.auger@linaro.org>
+ *
+ * This file is licensed under the terms of the GNU General Public
+ * License version 2.  This program is licensed "as is" without any
+ * warranty of any kind, whether express or implied.
+ */
+
+#ifndef _LINUX_MSI_DOORBELL_H
+#define _LINUX_MSI_DOORBELL_H
+
+#include <linux/irq.h>
+
+#ifdef CONFIG_MSI_DOORBELL
+
+/**
+ * msi_doorbell_register_global - allocate and register a global doorbell
+ * @base: physical base address of the global doorbell
+ * @size: size of the global doorbell
+ * @prot: protection/memory attributes
+ * @irq_remapping: is irq_remapping implemented for this doorbell
+ *
+ * Return: the newly allocated doorbell info handle or NULL if allocation
+ * failed
+ */
+struct msi_doorbell_info *
+msi_doorbell_register_global(phys_addr_t base, size_t size,
+			     int prot, bool irq_remapping);
+
+/**
+ * msi_doorbell_unregister_global - unregister a global doorbell
+ * @db: doorbell info to unregister
+ *
+ * remove the doorbell descriptor from the list of registered doorbells
+ * and deallocates it
+ */
+void msi_doorbell_unregister_global(struct msi_doorbell_info *db);
+
+#else
+
+static inline struct msi_doorbell_info *
+msi_doorbell_register_global(phys_addr_t base, size_t size,
+			     int prot, bool irq_remapping)
+{
+	return NULL;
+}
+
+static inline void
+msi_doorbell_unregister_global(struct msi_doorbell_info *db) {}
+
+#endif /* CONFIG_MSI_DOORBELL */
+
+#endif
