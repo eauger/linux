c59691ba1bf0717c1283d1a5965d3482c5a54d46
Eric Auger <eric.auger@redhat.com>
2016-09-12 12:09:38 +0000

    iommu: Introduce IOMMU_CAP_TRANSLATE_MSI capability
    Introduce a new IOMMU_CAP_TRANSLATE_MSI capability to check whether
    the IOMMU translates MSI write transactions. This capability will be
    checked on domain allocation to check the type of the domain that
    shall be used.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -97,6 +97,7 @@ enum iommu_cap {
 					   transactions */
 	IOMMU_CAP_INTR_REMAP,		/* IOMMU supports interrupt isolation */
 	IOMMU_CAP_NOEXEC,		/* IOMMU_NOEXEC flag */
+	IOMMU_CAP_TRANSLATE_MSI,	/* IOMMU translates MSI transactions */
 };
 
 /*
