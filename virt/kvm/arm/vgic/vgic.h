42aa5e6865c9ce0c81d7e3ca99b23920e0b3b35c
Vijaya Kumar K <Vijaya.Kumar@cavium.com>
2016-12-01 12:39:44 +0530

    arm/arm64: vgic: Introduce VENG0 and VENG1 fields to vmcr struct
    ICC_VMCR_EL2 supports virtual access to ICC_IGRPEN1_EL1.Enable
    and ICC_IGRPEN0_EL1.Enable fields. Add grpen0 and grpen1 member
    variables to struct vmcr to support read and write of these fields.
    
    Also refactor vgic_set_vmcr and vgic_get_vmcr() code.
    Drop ICH_VMCR_CTLR_SHIFT and ICH_VMCR_CTLR_MASK macros and instead
    use ICH_VMCR_EOI* and ICH_VMCR_CBPR* macros.
    
    Signed-off-by: Vijaya Kumar K <Vijaya.Kumar@cavium.com>
    Reviewed-by: Christoffer Dall <christoffer.dall@linaro.org>

--- virt/kvm/arm/vgic/vgic.h
+++ virt/kvm/arm/vgic/vgic.h
@@ -78,6 +78,9 @@ struct vgic_vmcr {
 	u32	abpr;
 	u32	bpr;
 	u32	pmr;
+	/* Below member variable are valid only for GICv3 */
+	u32	grpen0;
+	u32	grpen1;
 };
 
 struct vgic_irq *vgic_get_irq(struct kvm *kvm, struct kvm_vcpu *vcpu,
@@ -139,6 +142,8 @@ int vgic_v3_dist_uaccess(struct kvm_vcpu *vcpu, bool is_write,
 int vgic_v3_redist_uaccess(struct kvm_vcpu *vcpu, bool is_write,
 			 int offset, u32 *val);
 int kvm_register_vgic_device(unsigned long type);
+void vgic_set_vmcr(struct kvm_vcpu *vcpu, struct vgic_vmcr *vmcr);
+void vgic_get_vmcr(struct kvm_vcpu *vcpu, struct vgic_vmcr *vmcr);
 int vgic_lazy_init(struct kvm *kvm);
 int vgic_init(struct kvm *kvm);
 
