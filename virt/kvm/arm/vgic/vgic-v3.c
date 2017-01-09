504d40700bc02ff9b332fc13aba652c03b475628
Eric Auger <eric.auger@redhat.com>
2017-01-09 16:28:27 +0100

    KVM: arm64: vgic-v3: KVM_DEV_ARM_VGIC_SAVE_PENDING_TABLES
    This patch adds a new attribute to GICV3 KVM device
    KVM_DEV_ARM_VGIC_GRP_CTRL group. This Allows the userspace to
    flush all GICR pending tables into guest RAM. At the moment
    we do not offer any restore control as the sync is implicit.
    
    As we need the PENDBASER_ADDRESS() in vgic-v3, let's move its
    definition in the irqchip header. We restore the full length
    of the field, ie [51:16]. Same for PROPBASER_ADDRESS with full
    field length of [51:12].
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    
    ---
    v4 -> v5:
    - move pending table save code/ctrl into VGICv3 instead of ITS
    - remove useless gpa_t cast
    - use the same optimization as in its_sync_lpi_pending_table
    
    v3 -> v4:
    - remove the wrong comment about locking
    - pass kvm struct instead of its handle
    - add comment about restore method
    - remove GITR_PENDABASER.PTZ check
    - continue if target_vcpu == NULL
    - new locking strategy
    
    v1 -> v2:
    - do not care about the 1st KB which should be zeroed according to
      the spec.

--- virt/kvm/arm/vgic/vgic-v3.c
+++ virt/kvm/arm/vgic/vgic-v3.c
@@ -15,6 +15,7 @@
 #include <linux/irqchip/arm-gic-v3.h>
 #include <linux/kvm.h>
 #include <linux/kvm_host.h>
+//#include <linux/bitops.h>
 #include <kvm/arm_vgic.h>
 #include <asm/kvm_mmu.h>
 #include <asm/kvm_asm.h>
@@ -251,6 +252,59 @@ void vgic_v3_enable(struct kvm_vcpu *vcpu)
 	/* Get the show on the road... */
 	vgic_v3->vgic_hcr = ICH_HCR_EN;
 }
+
+/**
+ * vgic_its_save_pending_tables - Save the pending tables into guest RAM
+ * kvm lock and all vcpu lock must be held
+ */
+int vgic_v3_save_pending_tables(struct kvm *kvm)
+{
+	struct vgic_dist *dist = &kvm->arch.vgic;
+	int last_byte_offset = -1;
+	struct vgic_irq *irq;
+	int ret;
+
+	list_for_each_entry(irq, &dist->lpi_list_head, lpi_list) {
+		int byte_offset, bit_nr;
+		struct kvm_vcpu *vcpu;
+		gpa_t pendbase, ptr;
+		bool stored;
+		u8 val;
+
+		vcpu = irq->target_vcpu;
+		if (!vcpu)
+			continue;
+
+		pendbase = GICR_PENDBASER_ADDRESS(vcpu->arch.vgic_cpu.pendbaser);
+
+		byte_offset = irq->intid / BITS_PER_BYTE;
+		bit_nr = irq->intid % BITS_PER_BYTE;
+		ptr = pendbase + byte_offset;
+
+		if (byte_offset != last_byte_offset) {
+			ret = kvm_read_guest(kvm, ptr, &val, 1);
+			if (ret)
+				return ret;
+			last_byte_offset = byte_offset;
+		}
+
+		stored = val & bit_nr;
+		if (stored == irq->pending_latch)
+			continue;
+
+		if (irq->pending_latch)
+			val |= 1 << bit_nr;
+		else
+			val &= ~(1 << bit_nr);
+
+		ret = kvm_write_guest(kvm, ptr, &val, 1);
+		if (ret)
+			return ret;
+	}
+
+	return 0;
+}
+
 
 /* check for overlapping regions and for regions crossing the end of memory */
 static bool vgic_v3_check_base(struct kvm *kvm)
--- virt/kvm/arm/vgic/vgic.h
+++ virt/kvm/arm/vgic/vgic.h
@@ -177,6 +177,7 @@ void vgic_v3_get_vmcr(struct kvm_vcpu *vcpu, struct vgic_vmcr *vmcr);
 void vgic_v3_enable(struct kvm_vcpu *vcpu);
 int vgic_v3_probe(const struct gic_kvm_info *info);
 int vgic_v3_map_resources(struct kvm *kvm);
+int vgic_v3_save_pending_tables(struct kvm *kvm);
 int vgic_register_redist_iodevs(struct kvm *kvm, gpa_t dist_base_address);
 
 int vgic_register_its_iodevs(struct kvm *kvm);
