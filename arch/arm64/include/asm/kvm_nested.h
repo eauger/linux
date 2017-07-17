bd0f2f4f317922ccff42711490ecc4bb46db3198
Jintack Lim <jintack.lim@linaro.org>
2017-07-17 13:27:14 -0500

    KVM: arm64: nv: Respect virtual HCR_EL2.{NV,TSC) settings
    Forward traps due to HCR_EL2.{NV,TSC} bits to the virtual EL2 if
    they are not coming from the virtual EL2, corresponding to SMC
    and ERET instructions.
    
    Signed-off-by: Jintack Lim <jintack.lim@linaro.org>
    [Moved code to emulate-nested.c]
    Signed-off-by: Christoffer Dall <christoffer.dall@arm.com>
    [maz: drastically simplified]
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/kvm_nested.h
+++ arch/arm64/include/asm/kvm_nested.h
@@ -59,6 +59,7 @@ static inline u64 translate_ttbr0_el2_to_ttbr0_el1(u64 ttbr0)
 	return ttbr0 & ~GENMASK_ULL(63, 48);
 }
 
+extern bool forward_smc_trap(struct kvm_vcpu *vcpu);
 extern bool __check_nv_sr_forward(struct kvm_vcpu *vcpu);
 
 struct sys_reg_params;
