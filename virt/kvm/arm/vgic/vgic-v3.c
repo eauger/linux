009ed9af4d8768c5a0813d0261bb7e0307d1545d
Eric Auger <eric.auger@redhat.com>
2018-03-27 14:31:26 +0200

    KVM: arm/arm64: Helper to locate free rdist index
    We introduce vgic_v3_rdist_free_slot to help identifying
    where we can place a new 2x64KB redistributor.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>

--- virt/kvm/arm/vgic/vgic-v3.c
+++ virt/kvm/arm/vgic/vgic-v3.c
@@ -438,6 +438,23 @@ bool vgic_v3_check_base(struct kvm *kvm)
 	return false;
 }
 
+/**
+ * vgic_v3_rdist_free_slot - Look up registered rdist regions and identify one
+ * which has free space to put a new rdist regions
+ *
+ * If any, return this redist region handle, otherwise returns NULL.
+ */
+struct vgic_redist_region *vgic_v3_rdist_free_slot(struct list_head *rd_regions)
+{
+	struct vgic_redist_region *rdreg;
+
+	list_for_each_entry(rdreg, rd_regions, list) {
+		if (!vgic_v3_redist_region_full(rdreg))
+			return rdreg;
+	}
+	return NULL;
+}
+
 int vgic_v3_map_resources(struct kvm *kvm)
 {
 	int ret = 0;
