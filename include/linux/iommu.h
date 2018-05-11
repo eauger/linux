4e5e7e8b520fa30c0a6d1cc569af4707ab8e2e18
Jacob Pan <jacob.jun.pan@linux.intel.com>
2018-05-11 13:54:05 -0700

    iommu: introduce device fault report API
    Traditionally, device specific faults are detected and handled within
    their own device drivers. When IOMMU is enabled, faults such as DMA
    related transactions are detected by IOMMU. There is no generic
    reporting mechanism to report faults back to the in-kernel device
    driver or the guest OS in case of assigned devices.
    
    Faults detected by IOMMU is based on the transaction's source ID which
    can be reported at per device basis, regardless of the device type is a
    PCI device or not.
    
    The fault types include recoverable (e.g. page request) and
    unrecoverable faults(e.g. access error). In most cases, faults can be
    handled by IOMMU drivers internally. The primary use cases are as
    follows:
    1. page request fault originated from an SVM capable device that is
    assigned to guest via vIOMMU. In this case, the first level page tables
    are owned by the guest. Page request must be propagated to the guest to
    let guest OS fault in the pages then send page response. In this
    mechanism, the direct receiver of IOMMU fault notification is VFIO,
    which can relay notification events to QEMU or other user space
    software.
    
    2. faults need more subtle handling by device drivers. Other than
    simply invoke reset function, there are needs to let device driver
    handle the fault with a smaller impact.
    
    This patchset is intended to create a generic fault report API such
    that it can scale as follows:
    - all IOMMU types
    - PCI and non-PCI devices
    - recoverable and unrecoverable faults
    - VFIO and other other in kernel users
    - DMA & IRQ remapping (TBD)
    The original idea was brought up by David Woodhouse and discussions
    summarized at https://lwn.net/Articles/608914/.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Ashok Raj <ashok.raj@intel.com>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Eric Auger <eric.auger@redhat.com>
    [adapt to new iommu_fault fault field, test fault_param on
     iommu_unregister_device_fault_handler]

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -256,11 +256,13 @@ struct iommu_device {
  * - DMA remapping and IRQ remapping faults
  *
  * @fault: fault descriptor
+ * @list pending fault event list, used for tracking responses
  * @iommu_private: used by the IOMMU driver for storing fault-specific
  *                 data. Users should not modify this field before
  *                 sending the fault response.
  */
 struct iommu_fault_event {
+	struct list_head list;
 	struct iommu_fault fault;
 	u64 iommu_private;
 };
@@ -269,10 +271,13 @@ struct iommu_fault_event {
  * struct iommu_fault_param - per-device IOMMU fault data
  * @dev_fault_handler: Callback function to handle IOMMU faults at device level
  * @data: handler private data
- *
+ * @faults: holds the pending faults which needs response, e.g. page response.
+ * @lock: protect pending PRQ event list
  */
 struct iommu_fault_param {
 	iommu_dev_fault_handler_t handler;
+	struct list_head faults;
+	struct mutex lock;
 	void *data;
 };
 
@@ -286,6 +291,7 @@ struct iommu_fault_param {
  *	struct iommu_fwspec	*iommu_fwspec;
  */
 struct iommu_param {
+	struct mutex lock;
 	struct iommu_fault_param *fault_param;
 };
 
@@ -378,6 +384,15 @@ extern int iommu_group_register_notifier(struct iommu_group *group,
 					 struct notifier_block *nb);
 extern int iommu_group_unregister_notifier(struct iommu_group *group,
 					   struct notifier_block *nb);
+extern int iommu_register_device_fault_handler(struct device *dev,
+					iommu_dev_fault_handler_t handler,
+					void *data);
+
+extern int iommu_unregister_device_fault_handler(struct device *dev);
+
+extern int iommu_report_device_fault(struct device *dev,
+				     struct iommu_fault_event *evt);
+
 extern int iommu_group_id(struct iommu_group *group);
 extern struct iommu_group *iommu_group_get_for_dev(struct device *dev);
 extern struct iommu_domain *iommu_group_default_domain(struct iommu_group *);
@@ -656,6 +671,25 @@ static inline int iommu_group_unregister_notifier(struct iommu_group *group,
 						  struct notifier_block *nb)
 {
 	return 0;
+}
+
+static inline
+int iommu_register_device_fault_handler(struct device *dev,
+					iommu_dev_fault_handler_t handler,
+					void *data)
+{
+	return -ENODEV;
+}
+
+static inline int iommu_unregister_device_fault_handler(struct device *dev)
+{
+	return 0;
+}
+
+static inline
+int iommu_report_device_fault(struct device *dev, struct iommu_fault_event *evt)
+{
+	return -ENODEV;
 }
 
 static inline int iommu_group_id(struct iommu_group *group)
