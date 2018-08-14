99e6387818f70562704f622c9acb7aa12cc68bb4
Eric Auger <eric.auger@redhat.com>
2018-08-14 07:54:32 -0400

    iommu: Introduce bind_guest_msi
    On ARM, MSI are translated by the SMMU. An IOVA is allocated
    for each MSI doorbell. If both the host and the guest are exposed
    with SMMUs, we end up with 2 different IOVAs allocated by each.
    guest allocates an IOVA (gIOVA) to map onto the guest MSI
    doorbell (gDB). The Host allocates another IOVA (hIOVA) to map
    onto the physical doorbell (hDB).
    
    So we end up with 2 untied mappings:
             S1            S2
    gIOVA    ->    gDB
                  hIOVA    ->    gDB
    
    Currently the PCI device is programmed by the host with hIOVA
    as MSI doorbell. So this does not work.
    
    This patch introduces an API to pass gIOVA/gDB to the host so
    that gIOVA can be reused by the host instead of re-allocating
    a new IOVA. So the goal is to create the following nested mapping:
    
             S1            S2
    gIOVA    ->    gDB     ->    hDB
    
    and program the PCI device with gIOVA MSI doorbell.
    
    Signed-off-by: Eric Auger <eric.auger@redhat.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -247,6 +247,9 @@ struct iommu_ops {
 	int (*tlb_invalidate)(struct iommu_domain *domain, struct device *dev,
 			      struct iommu_tlb_invalidate_info *inv_info);
 
+	int (*bind_guest_msi)(struct iommu_domain *domain,
+			      struct iommu_guest_msi_binding *binding);
+
 	unsigned long pgsize_bitmap;
 };
 
@@ -315,6 +318,8 @@ extern void iommu_unbind_guest_stage(struct iommu_domain *domain,
 extern int iommu_tlb_invalidate(struct iommu_domain *domain,
 				struct device *dev,
 				struct iommu_tlb_invalidate_info *inv_info);
+extern int iommu_bind_guest_msi(struct iommu_domain *domain,
+				struct iommu_guest_msi_binding *binding);
 
 extern struct iommu_domain *iommu_get_domain_for_dev(struct device *dev);
 extern int iommu_map(struct iommu_domain *domain, unsigned long iova,
@@ -728,6 +733,13 @@ void iommu_unbind_guest_stage(struct iommu_domain *domain, struct device *dev)
 }
 static inline int iommu_tlb_invalidate(struct iommu_domain *domain,
 		struct device *dev, struct iommu_tlb_invalidate_info *inv_info)
+{
+	return -ENODEV;
+}
+
+static inline
+int iommu_bind_guest_msi(struct iommu_domain *domain,
+			 struct iommu_guest_msi_binding *binding)
 {
 	return -ENODEV;
 }
