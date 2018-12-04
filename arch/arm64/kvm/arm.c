8511c26db5302464e7ee7643bf80b4647eb8185c
Marc Zyngier <maz@kernel.org>
2018-12-04 15:42:29 +0000

    KVM: arm64: nv: Load timer before the GIC
    In order for vgic_v3_load_nested to be able to observe which timer
    interrupts have the HW bit set for the current context, the timers
    must have been loaded in the new mode and the right timer mapped
    to their corresponding HW IRQs.
    
    At the moment, we load the GIC first, meaning that timer interrupts
    injected to an L2 guest will never have the HW bit set (we see the
    old configuration).
    
    Swapping the two loads solves this particular problem.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -438,8 +438,8 @@ void kvm_arch_vcpu_load(struct kvm_vcpu *vcpu, int cpu)
 
 	vcpu->cpu = cpu;
 
-	kvm_vgic_load(vcpu);
 	kvm_timer_vcpu_load(vcpu);
+	kvm_vgic_load(vcpu);
 	if (has_vhe())
 		kvm_vcpu_load_sysregs_vhe(vcpu);
 	kvm_arch_vcpu_load_fp(vcpu);
