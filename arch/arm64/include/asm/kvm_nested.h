a778c6f1e2c8aea5f327f0da85c6c758a4c87eb4
Marc Zyngier <maz@kernel.org>
2020-09-08 16:52:07 +0100

    KVM: arm64: nv: Tag shadow S2 entries with nested level
    Populate bits [56:55] of the leaf entry with the level provided
    by the guest's S2 translation.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/include/asm/kvm_nested.h
+++ arch/arm64/include/asm/kvm_nested.h
@@ -5,6 +5,7 @@
 #include <linux/bitfield.h>
 #include <linux/kvm_host.h>
 #include <asm/kvm_emulate.h>
+#include <asm/kvm_pgtable.h>
 
 static inline bool vcpu_has_nv(const struct kvm_vcpu *vcpu)
 {
@@ -133,5 +134,10 @@ unsigned long compute_tlb_inval_range(struct kvm_s2_mmu *mmu, u64 val);
 int kvm_init_nv_sysregs(struct kvm *kvm);
 
 #define KVM_NV_GUEST_MAP_SZ	(KVM_PGTABLE_PROT_SW1 | KVM_PGTABLE_PROT_SW0)
+
+static inline u64 kvm_encode_nested_level(struct kvm_s2_trans *trans)
+{
+	return FIELD_PREP(KVM_NV_GUEST_MAP_SZ, trans->level);
+}
 
 #endif /* __ARM64_KVM_NESTED_H */
