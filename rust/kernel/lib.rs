7b883dced66bf470ec035bd654decfb91428489e
Asahi Lina <lina@asahilina.net>
2023-02-05 20:29:01 +0900

    rust: Add a Sealed trait
    Some traits exposed by the kernel crate may not be intended to be
    implemented by downstream modules. Add a Sealed trait to allow avoiding
    this using the sealed trait pattern.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/kernel/lib.rs
+++ rust/kernel/lib.rs
@@ -50,6 +50,11 @@ pub use macros;
 #[doc(hidden)]
 pub use build_error::build_error;
 
+pub(crate) mod private {
+    #[allow(unreachable_pub)]
+    pub trait Sealed {}
+}
+
 /// Prefix to appear before log messages printed from within the `kernel` crate.
 const __LOG_PREFIX: &[u8] = b"rust_kernel\0";
 
