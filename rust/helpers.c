708449fe6111cdf55389ca459539e9b689ea8223
Asahi Lina <lina@asahilina.net>
2023-02-11 17:08:36 +0900

    rust: drm: sched: Add GPU scheduler abstraction
    The GPU scheduler manages scheduling GPU jobs and dependencies between
    them. This Rust abstraction allows Rust DRM drivers to use this
    functionality.
    
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/helpers.c
+++ rust/helpers.c
@@ -385,6 +385,12 @@ void rust_helper___spin_lock_init(spinlock_t *lock, const char *name,
 }
 EXPORT_SYMBOL_GPL(rust_helper___spin_lock_init);
 
+unsigned long rust_helper_msecs_to_jiffies(const unsigned int m)
+{
+	return msecs_to_jiffies(m);
+}
+EXPORT_SYMBOL_GPL(rust_helper_msecs_to_jiffies);
+
 #ifdef CONFIG_DMA_SHARED_BUFFER
 
 void rust_helper_dma_fence_get(struct dma_fence *fence)
