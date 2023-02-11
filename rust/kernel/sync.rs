edebe114e112d47172b35a22639a946fc7e18cc2
Asahi Lina <lina@asahilina.net>
2023-02-11 16:14:16 +0900

    rust: sync: Add LockClassKey
    Add a simple wrapper around `lock_class_key`, to allow safe Rust code to
    create and use lockdep class keys.
    
    Co-developed-by: Wedson Almeida Filho <wedsonaf@google.com>
    Signed-off-by: Wedson Almeida Filho <wedsonaf@google.com>
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- rust/kernel/sync.rs
+++ rust/kernel/sync.rs
@@ -7,4 +7,26 @@
 
 mod arc;
 
+use core::{cell::UnsafeCell, mem::MaybeUninit};
+
 pub use arc::{Arc, ArcBorrow, UniqueArc};
+
+/// Represents a lockdep class. It's a wrapper around C's `lock_class_key`.
+#[repr(transparent)]
+pub struct LockClassKey(UnsafeCell<MaybeUninit<bindings::lock_class_key>>);
+
+// SAFETY: This is a wrapper around a lock class key, so it is safe to use references to it from
+// any thread.
+unsafe impl Sync for LockClassKey {}
+
+impl LockClassKey {
+    /// Creates a new lock class key.
+    pub const fn new() -> Self {
+        Self(UnsafeCell::new(MaybeUninit::uninit()))
+    }
+
+    #[allow(dead_code)]
+    pub(crate) fn get(&self) -> *mut bindings::lock_class_key {
+        self.0.get().cast()
+    }
+}
