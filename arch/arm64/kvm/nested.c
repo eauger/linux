ac0f84505b2bf6a6569a4656344a9709e05d0380
Marc Zyngier <maz@kernel.org>
2024-12-02 12:08:11 +0000

    KVM: arm64: nv: Sanitise ICH_HCR_EL2 accesses
    As ICH_HCR_EL2 is a VNCR accessor when runnintg NV, add some
    sanitising to what gets written. Crucially, mark TDIR as RES0
    if the HW doesn't support it (unlikely, but hey...), as well
    as anything GICv4 related, since we only expose a GICv3 to the
    uest.
    
    Signed-off-by: Marc Zyngier <maz@kernel.org>

--- arch/arm64/kvm/nested.c
+++ arch/arm64/kvm/nested.c
@@ -1271,6 +1271,15 @@ int kvm_init_nv_sysregs(struct kvm *kvm)
 		res0 |= MDCR_EL2_EnSTEPOP;
 	set_sysreg_masks(kvm, MDCR_EL2, res0, res1);
 
+	/* ICH_HCR_EL2 */
+	res0 = ICH_HCR_EL2_RES0;
+	res1 = ICH_HCR_EL2_RES1;
+	if (!(kvm_vgic_global_state.ich_vtr_el2 & ICH_VTR_EL2_TDS))
+		res0 |= ICH_HCR_EL2_TDIR;
+	/* No GICv4 is presented to the guest */
+	res0 |= ICH_HCR_EL2_DVIM | ICH_HCR_EL2_vSGIEOICount;
+	set_sysreg_masks(kvm, ICH_HCR_EL2, res0, res1);
+
 	return 0;
 }
 
